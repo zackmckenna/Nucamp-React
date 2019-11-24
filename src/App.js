93b68fa4c56995756196a6dad1a5b55ca5918d6e
Zack McKenna <zackmckenna@gmail.com>
2019-11-24 13:32:37 -0500

    Added fonts and created cards components for campsites

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent'
+import { CAMPSITES } from './shared/campsites';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
+
     render() {
         return (
             <div className="App">
@@ -12,7 +20,7 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites}/>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
 
 import * as serviceWorker from './serviceWorker';
 
