7bd7b124a6ca36371c16f9ee918b02a465668d62
Zack McKenna <zackmckenna@gmail.com>
2019-12-02 16:42:01 -0500

    React Router

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}){
+function RenderDirectoryItem({ campsite }){
     return (
-        <Card onClick={() => onClick(campsite.id)}>
+        <Card >
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
