84a40015506106d86b714f7faa412343b4de60b5
Zack McKenna <zackmckenna@gmail.com>
2019-12-02 16:13:11 -0500

    Added HeaderComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './DirectoryComponent'
-import CampsiteInfo from './CampsiteInfoComponent'
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Header from './HeaderComponent'
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
@@ -20,11 +20,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
             </div>
